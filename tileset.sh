37776d841a1c937181bb60ecaedd24b05b736cca
seandoesgis <slawrence@dvrpc.org>
2024-09-20 16:23:31 -0400

    fix: add id to tilesets

--- tileset.sh
+++ tileset.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 # optimized for MacOS, requires tippecanoe
 
-# db creds
+# db connection info from .env
+required_vars=("USER" "PASSWORD" "HOST" "PORT")
+for var in "${required_vars[@]}"; do
+    if [ -z "${!var}" ]; then
+        echo "Error: $var is not set in the .env file"
+        exit 1
+    fi
+done
+
 DB_NAME="eta"
-DB_USER=""
-DB_PASSWORD=""
-DB_HOST="localhost"
-DB_PORT="5432"
 
 OUTPUT_DIR="$HOME/gis-eta/output"
 
@@ -14,7 +18,7 @@ OUTPUT_DIR="$HOME/gis-eta/output"
 pg_to_geojson() {
     local query="$1"
     local output_file="$2"
-    local geom_column="${3:-geometry}"  # Default to 'geom' if not specified
+    local geom_column="${3:-geometry}"
     
     local wrapped_query="
     SELECT jsonb_build_object(
@@ -30,15 +34,15 @@ pg_to_geojson() {
       FROM ($query) row
     ) features"
     
-    PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -p $DB_PORT -d $DB_NAME -U $DB_USER -c "COPY ($wrapped_query) TO STDOUT" > "$output_file"
+    PGPASSWORD=$PASSWORD psql -h $HOST -p $PORT -d $DB_NAME -U $USER -c "COPY ($wrapped_query) TO STDOUT" > "$output_file"
 }
 
 mkdir -p "$OUTPUT_DIR"
 
-pg_to_geojson "SELECT * FROM output.output" "$OUTPUT_DIR/output.geojson"
-pg_to_geojson "SELECT id, stop_id, gtfs, geom as geometry FROM network.transit_poi_isochrones" "$OUTPUT_DIR/walksheds.geojson"
-pg_to_geojson "SELECT s.stop_id, s.gtfs, s.geom as geometry FROM output.stops_w_departs s, input.census_blockgroups cb where st_intersects(s.geom,cb.geometry)" "$OUTPUT_DIR/transitstops.geojson"
-pg_to_geojson "SELECT es.* FROM output.es_point_locations es, input.census_blockgroups cb where st_intersects(es.geometry,cb.geometry)" "$OUTPUT_DIR/es.geojson"
+pg_to_geojson "SELECT ROW_NUMBER() OVER () AS id, * FROM output.output" "$OUTPUT_DIR/output.geojson"
+pg_to_geojson "SELECT ROW_NUMBER() OVER () AS id, stop_id, gtfs, geom as geometry FROM network.transit_poi_isochrones" "$OUTPUT_DIR/walksheds.geojson"
+pg_to_geojson "SELECT ROW_NUMBER() OVER () AS id, s.stop_id, s.gtfs, s.geom as geometry FROM output.stops_w_departs s, input.census_blockgroups cb where st_intersects(s.geom,cb.geometry)" "$OUTPUT_DIR/transitstops.geojson"
+pg_to_geojson "SELECT ROW_NUMBER() OVER () AS id, es.* FROM output.es_point_locations es, input.census_blockgroups cb where st_intersects(es.geometry,cb.geometry)" "$OUTPUT_DIR/es.geojson"
 
 tippecanoe -o $OUTPUT_DIR/eta_score.mbtiles -l eta_score -f -r1 -pk -pf $OUTPUT_DIR/output.geojson
 tippecanoe -o $OUTPUT_DIR/walksheds.mbtiles -l walksheds -f -r1 -pk -pf $OUTPUT_DIR/walksheds.geojson
